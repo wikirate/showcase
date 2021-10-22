8a853ae794ffb9925a3e4b47a26a8b1f395319b3
vasgat <vasgat@iti.gr>
2021-10-22 09:45:41 +0200

    add CompanyResolver service

---
+++ src/app/services/company-resolver.service.ts
@@ -0,0 +1,18 @@
+import {Company} from "../models/company.model";
+import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from "@angular/router";
+import {Observable} from "rxjs";
+import {ApparelService} from "./apparel.service";
+import {Injectable} from "@angular/core";
+
+@Injectable()
+export class CompanyResolver implements Resolve<Company> {
+  constructor(private apparelService: ApparelService) {
+  }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+    Observable<Company> | Promise<Company> | Company {
+    // @ts-ignore
+    return this.apparelService.getCompany(+route.params['id']);
+  }
+
+}
