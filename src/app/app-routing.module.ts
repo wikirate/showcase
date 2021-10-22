37e7a079559879928b8e799a201a77f50ee69b25
vasgat <vasgat@iti.gr>
2021-10-22 09:54:22 +0200

    updating app.module.ts and app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,29 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {DynamicReportComponent} from "./dynamic-report/dynamic-report.component";
 import {AboutComponent} from "./about/about.component";
+import {CompanyResolver} from "./services/company-resolver.service";
+import {FormsModule} from "@angular/forms";
+import {BrowserModule} from "@angular/platform-browser";
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 const routes: Routes = [
   {path: '', redirectTo: 'apparel_top_100/reports/0/latest', pathMatch: 'full'},
   {
-    path: 'apparel_top_100/reports/:id/:year', component: DynamicReportComponent
+    path: 'apparel_top_100/reports/:id/:year', component: DynamicReportComponent, resolve: {company: CompanyResolver}
   },
   {path: 'apparel_top_100/about', component: AboutComponent},
+  { path: '**', redirectTo: '/apparel_top_100/reports/0/latest', pathMatch: 'full' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    RouterModule.forRoot(
+      routes,
+      {scrollPositionRestoration: 'enabled'}
+    )],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import {SuppliersMapComponent} from './dynamic-report/suppliers-map/suppliers-ma
 import {NetworkGraphComponent} from './dynamic-report/network-graph/network-graph.component';
 import {DynamicReportComponent} from './dynamic-report/dynamic-report.component';
 import { MarketCapMapComponent } from './dynamic-report/market-cap-map/market-cap-map.component';
-import { SummaryComponent } from './dynamic-report/summary/summary.component';
 import { SupplierListsOnWikirateComponent } from './dynamic-report/supplier-lists-on-wikirate/supplier-lists-on-wikirate.component';
 import { NumberOfSuppliersPerCompanyComponent } from './dynamic-report/number-of-suppliers-per-company/number-of-suppliers-per-company.component';
 import {RouterModule} from "@angular/router";
@@ -19,9 +18,12 @@ import { FooterComponent } from './footer/footer.component';
 import { MoreDataComponent } from './dynamic-report/more-data/more-data.component';
 import { AboutComponent } from './about/about.component';
 import {ApparelService} from "./services/apparel.service";
-import { FilterPipe } from './filter.pipe';
+import { FilterPipe } from './pipes/filter.pipe';
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import { TextHighlightDirective } from './directives/text-highlight.directive';
+import {CompanyResolver} from "./services/company-resolver.service";
+import {BigNumberFormatPipe} from "./pipes/big-number-format.pipe";
+import {NumFormatPipe} from "./pipes/num-format.pipe";
 
 @NgModule({
   declarations: [
@@ -32,13 +34,14 @@ import { TextHighlightDirective } from './directives/text-highlight.directive';
     NetworkGraphComponent,
     DynamicReportComponent,
     MarketCapMapComponent,
-    SummaryComponent,
     SupplierListsOnWikirateComponent,
     NumberOfSuppliersPerCompanyComponent,
     FooterComponent,
     MoreDataComponent,
     AboutComponent,
     FilterPipe,
+    BigNumberFormatPipe,
+    NumFormatPipe,
     TextHighlightDirective
   ],
   imports: [
@@ -49,7 +52,7 @@ import { TextHighlightDirective } from './directives/text-highlight.directive';
     RouterModule,
     NgbModule
   ],
-  providers: [ApparelService],
+  providers: [ApparelService, CompanyResolver],
   bootstrap: [AppComponent]
 })
 export class AppModule {
