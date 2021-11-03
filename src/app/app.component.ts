2cf63a149e2b17cde1b03e1a2f99f9979ea2e169
vasgat <vasgat@iti.gr>
2021-11-03 10:17:56 +0100

    adding google analytics tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,44 @@
 import {AfterViewInit, Component, HostListener, OnInit} from '@angular/core';
 import {ViewportScroller} from "@angular/common";
+import {NavigationEnd, Router} from "@angular/router";
+import {filter} from "rxjs/operators";
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   pageYoffset = 0;
 
   @HostListener('window:scroll', ['$event']) onScroll(event: any) {
     this.pageYoffset = window.pageYOffset;
   }
 
-  constructor(private scroll: ViewportScroller) {
+  constructor(private scroll: ViewportScroller, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.setUpAnalytics();
   }
 
   scrollToTop() {
     this.scroll.scrollToPosition([0, 0]);
+  }
+
+  setUpAnalytics() {
+
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd))
+      // @ts-ignore
+      .subscribe((event: NavigationEnd) => {
+        gtag('config', 'G-KP6VMMHLSE',
+          {
+            page_path: event.urlAfterRedirects
+          }
+        );
+      });
   }
 }
 
