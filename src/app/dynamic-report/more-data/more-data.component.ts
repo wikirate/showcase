7cd5d42b9cbb2dea2b92e7a488c3b247573165e4
vasgat <vasgat@iti.gr>
2021-11-03 15:20:24 +0100

    final edits

--- src/app/dynamic-report/more-data/more-data.component.ts
+++ src/app/dynamic-report/more-data/more-data.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'more-data',
@@ -7,9 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoreDataComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
-
 }
